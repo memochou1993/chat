64452ed71d03b112beae676e3fae6f9eff2fbaf0
William Chou <williamchou@starlux-airlines.com>
2020-02-20 14:37:53 +0800

    Add Header component

--- resources/js/src/App.js
+++ resources/js/src/App.js
@@ -1,16 +1,19 @@
 import React, { useEffect } from 'react';
-import './App.css';
-import { connect, sendMessage } from "./api";
+import Header from './components/Header';
+import { connect, sendMessage } from './api';
 
 const App = () => {
   useEffect(() => {
     connect();
   }, []);
+
   const send = () => {
     sendMessage('Hello!');
   };
+
   return (
     <div className="App">
+      <Header />
       <button onClick={send}>Hit</button>
     </div>
   );
