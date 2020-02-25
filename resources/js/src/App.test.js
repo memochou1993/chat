cba66eea52b2e6d8dda58c39c7e886480b177571
Memo Chou <memochou1993@gmail.com>
2020-02-26 01:01:39 +0800

    Modify test file

--- resources/js/src/App.test.js
+++ resources/js/src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders hit button', () => {
+test('renders app title', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/hit/i);
+  const linkElement = getByText(/Chat App/i);
   expect(linkElement).toBeInTheDocument();
 });
